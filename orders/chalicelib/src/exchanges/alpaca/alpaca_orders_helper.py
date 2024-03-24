f8493ea236bbe01c4e03a42380f59b89bf0c5d4e
Joseph Mendezona <joseph.mendezona@pwc.com>
2024-03-24 19:42:52 +0100

    refactor alpaca_calculate_profit_loss

--- orders/chalicelib/src/exchanges/alpaca/alpaca_orders_helper.py
+++ orders/chalicelib/src/exchanges/alpaca/alpaca_orders_helper.py
@@ -1,6 +1,11 @@
+from decimal import Decimal
+from typing import List
+
 from alpaca.common import RawData
 from alpaca.trading.client import TradingClient
-from alpaca.trading.models import Asset
+from alpaca.trading.enums import OrderSide, QueryOrderStatus
+from alpaca.trading.models import Asset, Order
+from alpaca.trading.requests import GetOrdersRequest
 from chalicelib.src.exchanges.alpaca.alpaca_account_utils import (
     alpaca_get_credentials,
 )
@@ -16,6 +21,18 @@ def alpaca_is_asset_fractionable(
     symbol: str,
     account: str = alpaca_trading_account_name_live,
 ) -> bool:
+    """
+    Checks if asset is fractionable, or if only whole orders can be submitted
+
+    Parameters:
+    - symbol: Symbol to check if asset is fractionable
+    - account: Account to use to check if asset if fractionable
+
+    Returns:
+    - A Boolean, true being asset is fractionable (eg 0.10 quantity is
+    accepted)
+    """
+
     credentials: AlpacaAccountCredentials = alpaca_get_credentials(account)
     if credentials:
         client = TradingClient(
@@ -25,8 +42,78 @@ def alpaca_is_asset_fractionable(
         )
         try:
             asset: Asset | RawData = client.get_asset(symbol)
-            print("assetreturn", asset)
-            print("asset fractionable: ", asset.fractionable)
+            print("asset fractionable:", asset.fractionable)
             return asset.fractionable
         except Exception as e:
             print(f"Error fetching asset information: {e}")
+
+
+def alpaca_calculate_profit_loss(
+    symbol: str, account: str = alpaca_trading_account_name_live
+) -> Decimal:
+    """
+    Calculate the profit/loss amount on an asset's last trade. Looks at last
+    open and close of an asset
+
+    Parameters:
+    - symbol: Symbol to check if asset is fractionable
+    - account: Account to use to check if asset if fractionable
+
+    Returns:
+    - A Decimal, a negative or positive number based on profit or loss
+    calculation
+    """
+
+    credentials: AlpacaAccountCredentials = alpaca_get_credentials(account)
+    trading_client = TradingClient(
+        api_key=credentials["key"],
+        secret_key=credentials["secret"],
+        paper=credentials["paper"],
+    )
+
+    # Fetch the most recent orders, considering a reasonable limit
+    orders_request = GetOrdersRequest(
+        status=QueryOrderStatus.CLOSED, symbols=[symbol], limit=5
+    )
+    orders: List[Order] = trading_client.get_orders(filter=orders_request)
+
+    # Find the most recent sell order
+    recent_sell_order = next(
+        (order for order in reversed(orders) if order.side == OrderSide.SELL),
+        None,
+    )
+    if not recent_sell_order:
+        raise ValueError("No recent sell order found.")
+
+    sell_quantity_needed = Decimal(recent_sell_order.filled_qty)
+    sell_price = Decimal(recent_sell_order.filled_avg_price)
+    accumulated_buy_quantity = Decimal("0")
+    total_buy_cost = Decimal("0")
+
+    print("ordersordersorders", orders)
+    # Accumulate buy orders starting from the most recent
+    for order in reversed(orders):
+        if order.side == OrderSide.BUY:
+            buy_quantity = Decimal(order.filled_qty)
+            buy_price = Decimal(order.filled_avg_price)
+
+            # Determine how much of this order to use
+            quantity_to_use = min(
+                buy_quantity,
+                sell_quantity_needed - accumulated_buy_quantity,
+            )
+            total_buy_cost += quantity_to_use * buy_price
+            accumulated_buy_quantity += quantity_to_use
+
+            if accumulated_buy_quantity >= sell_quantity_needed:
+                break
+
+    if accumulated_buy_quantity < sell_quantity_needed:
+        raise ValueError("Not enough buy orders to match the sell quantity.")
+
+    # Calculate profit or loss
+    average_buy_price = total_buy_cost / accumulated_buy_quantity
+    profit_loss = (sell_price - average_buy_price) * sell_quantity_needed
+
+    print(f"Profit/loss: {profit_loss}")
+    return profit_loss
--- orders/chalicelib/src/exchanges/alpaca/tests/alpaca_orders_helper_test.py
+++ orders/chalicelib/src/exchanges/alpaca/tests/alpaca_orders_helper_test.py
@@ -1,8 +1,17 @@
 import uuid
+from decimal import Decimal
+from unittest.mock import MagicMock, patch
 
 import pytest
 import requests_mock
 from alpaca.trading.client import TradingClient
+from alpaca.trading.enums import OrderSide
+from chalicelib.src.exchanges.alpaca.alpaca_orders_helper import (
+    alpaca_calculate_profit_loss,
+)
+from chalicelib.src.exchanges.alpaca.alpaca_types import (
+    AlpacaAccountCredentials,
+)
 
 
 @pytest.fixture
@@ -29,7 +38,7 @@ def mock_requests():
         yield m
 
 
-def test_get_asset_success(asset_api_response, mock_requests):
+def test_is_asset_fractionable(asset_api_response, mock_requests):
     asset_symbol = "AAPL"
     api_url = f"https://paper-api.alpaca.markets/v2/assets/{asset_symbol}"
     mock_requests.get(api_url, json=asset_api_response)
@@ -39,3 +48,119 @@ def test_get_asset_success(asset_api_response, mock_requests):
 
     assert asset.symbol == asset_api_response["symbol"]
     assert asset.fractionable is asset_api_response["fractionable"]
+
+
+@pytest.fixture
+def mock_credentials():
+    """Fixture for mocking Alpaca credentials."""
+    return AlpacaAccountCredentials(
+        key="fake_key", secret="fake_secret", paper=True
+    )
+
+
+@pytest.fixture
+def mock_orders():
+    """Fixture for mocking a list of Order objects."""
+    return [
+        # Mock a sell order
+        MagicMock(
+            side=OrderSide.SELL, filled_qty="10", filled_avg_price="200"
+        ),
+        # Mock a buy order that matches the sell quantity
+        MagicMock(side=OrderSide.BUY, filled_qty="10", filled_avg_price="150"),
+    ]
+
+
+@patch("alpaca.trading.client.TradingClient")
+@patch(
+    "chalicelib.src.exchanges.alpaca.alpaca_account_utils.alpaca_get_credentials"
+)
+def alpaca_calculate_profit_loss_test_profit_scenario(
+    mock_get_credentials, mock_TradingClient, mock_credentials, mock_orders
+):
+    mock_credentials = {
+        "key": "fake_key",
+        "paper": True,
+        "secret": "fake_secret",
+    }
+    mock_get_credentials.return_value = mock_credentials
+    mock_orders = [
+        MagicMock(
+            side=OrderSide.BUY,
+            symbol="AAPL",
+            filled_qty="10",
+            filled_avg_price="150",
+        ),
+        MagicMock(
+            side=OrderSide.SELL,
+            symbol="AAPL",
+            filled_qty="10",
+            filled_avg_price="200",
+        ),
+    ]
+    mock_TradingClient.return_value.get_orders.return_value = mock_orders
+
+    profit_loss = alpaca_calculate_profit_loss("AAPL")
+    assert profit_loss == Decimal("500"), "Expected profit of 500 not met"
+
+
+@patch("alpaca.trading.client.TradingClient")
+@patch(
+    "chalicelib.src.exchanges.alpaca.alpaca_account_utils.alpaca_get_credentials"
+)
+def alpaca_calculate_profit_loss_test_loss_scenario(
+    mock_get_credentials, mock_TradingClient, mock_credentials, mock_orders
+):
+    """Test case for a loss scenario."""
+    # Adjust the mock_orders for a loss scenario
+    mock_orders[1].filled_avg_price = "250"
+    mock_get_credentials.return_value = mock_credentials
+    mock_TradingClient.return_value.get_orders.return_value = mock_orders
+
+    profit_loss = alpaca_calculate_profit_loss("AAPL")
+    assert profit_loss == Decimal(
+        "-500"
+    ), "Loss should be -500 for the given orders"
+
+
+@patch("alpaca.trading.client.TradingClient")
+@patch(
+    "chalicelib.src.exchanges.alpaca.alpaca_account_utils.alpaca_get_credentials"
+)
+def alpaca_calculate_profit_loss_test_no_sell_order_found(
+    mock_get_credentials, mock_TradingClient, mock_credentials
+):
+    """Test case for no sell order found."""
+    mock_get_credentials.return_value = mock_credentials
+    # Return only buy orders
+    mock_orders = [
+        MagicMock(side=OrderSide.BUY, filled_qty="10", filled_avg_price="150")
+    ]
+    mock_TradingClient.return_value.get_orders.return_value = mock_orders
+
+    with pytest.raises(ValueError, match="No recent sell order found."):
+        alpaca_calculate_profit_loss("AAPL")
+
+
+@patch("alpaca.trading.client.TradingClient")
+@patch(
+    "chalicelib.src.exchanges.alpaca.alpaca_account_utils.alpaca_get_credentials"
+)
+def alpaca_calculate_profit_loss_test_not_enough_buy_orders(
+    mock_get_credentials, mock_TradingClient, mock_credentials
+):
+    """Test case for not having enough buy orders to match the sell quantity."""
+    mock_get_credentials.return_value = mock_credentials
+    # Return one sell order and not enough buy quantity
+    mock_orders = [
+        MagicMock(
+            side=OrderSide.SELL, filled_qty="10", filled_avg_price="200"
+        ),
+        MagicMock(side=OrderSide.BUY, filled_qty="5", filled_avg_price="150"),
+    ]
+    mock_TradingClient.return_value.get_orders.return_value = mock_orders
+
+    with pytest.raises(
+        ValueError, match="Not enough buy orders to match the sell quantity."
+    ):
+        alpaca_calculate_profit_loss("AAPL")
